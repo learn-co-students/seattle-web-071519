818ce453351b324b728d2ad150629f27787b1057
thejamilah <jamilahmcw@gmail.com>
2019-07-23 14:29:49 -0700

    update

--- 06-Code-Challenge-Review/lib/Purchase.rb
+++ 06-Code-Challenge-Review/lib/Purchase.rb
@@ -1,3 +1,16 @@
 class Purchase
-  #Your code goes here!
+  attr_reader :game, :steam_user, :purchase_date
+  
+  @@all = []
+  
+  def initialize(steam_user, game, purchase_date = Time.now)
+    @steam_user = steam_user
+    @game = game
+    @purchase_date = purchase_date
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
